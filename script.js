a3e02c7de131209655edadcbda53d044c10243d7
xaviguasch <xaviguasch@gmail.com>
2020-02-13 12:51:22 +0100

    added display function to show the persons

--- script.js
+++ script.js
@@ -7,10 +7,6 @@ const calculateWealthBtn = document.querySelector('#calculate-wealth')
 
 let data = []
 
-getRandomUser()
-getRandomUser()
-getRandomUser()
-
 // fetch random user and add money
 async function getRandomUser() {
   fetch('https://randomuser.me/api')
@@ -32,4 +28,28 @@ async function getRandomUser() {
 // Add the new object to data array
 function addData(obj) {
   data.push(obj)
+
+  updateDOM()
 }
+
+function updateDOM(providedData = data) {
+  //Clear main div
+
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>'
+
+  providedData.forEach(item => {
+    const element = document.createElement('div')
+    element.classList.add('person')
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`
+
+    main.appendChild(element)
+  })
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,')
+}
+
+// Event listeners
+addUserBtn.addEventListener('click', getRandomUser)
